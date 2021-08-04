4d373bffa2a8f359fb050901768f0da3e80f0c1c
inni0122 <suve1y34@gmail.com>
2021-08-04 22:34:16 +0900

    application 실행 할때 자동으로 적용되는 웹 보안 설정 끄기

--- blog/src/main/java/com/blog/BlogApplication.java
+++ blog/src/main/java/com/blog/BlogApplication.java
@@ -2,12 +2,12 @@ package com.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
-
 }
