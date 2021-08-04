265bf065d330fff0b16336d6234ccb1d5c81b2be
inni0122 <suve1y34@gmail.com>
2021-08-05 03:16:50 +0900

    user 권한 부분 수정, repository 생성, insert 테스트 하는 중

--- blog/src/main/java/com/blog/model/User.java
+++ blog/src/main/java/com/blog/model/User.java
@@ -4,11 +4,12 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.AllArgsConstructor;
@@ -26,18 +27,19 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
-	@Column(nullable = false, length = 100)
+	@Column(nullable = false, length = 30, unique = true)
 	private String username;
 	
-	@Column(nullable = false, length = 50)
+	@Column(nullable = false, length = 100)
 	private String password;
 	
 	@Column(nullable = false, length = 50)
 	private String email;
 	
-	@ColumnDefault("'user'")
-	private String role;
+	@Enumerated(EnumType.STRING)
+	private RoleType role;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
+
 }
