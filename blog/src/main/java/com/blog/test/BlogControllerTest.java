89432581e034fde035e6a91ac49ffedb68c4dc2f
inni0122 <suve1y34@gmail.com>
2021-08-04 22:11:01 +0900

    프로젝트 기본 세팅, DB 연동 작업, 테스트 화면 띄우기

---
+++ blog/src/main/java/com/blog/test/BlogControllerTest.java
@@ -0,0 +1,13 @@
+package com.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+
+	@GetMapping("/test/hello")
+	public String helloWorld() {
+		return "<h1>hello world</h1>";
+	}
+}
